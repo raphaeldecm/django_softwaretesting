7ef5d4d8b40e6705e579607c4ce51dad62ddfa91
Raphael Muniz <raphael@copin.ufcg.edu.br>
2022-11-11 16:45:49 -0300

    settings update

--- converter/settings.py
+++ converter/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_saiui6u!8cj-ba4-%2z$%=_b029(r3z+mfpji99&1)en@ae4m'
+SECRET_KEY = config("SECRET_KEY")
+##'django-insecure-_saiui6u!8cj-ba4-%2z$%=_b029(r3z+mfpji99&1)en@ae4m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
